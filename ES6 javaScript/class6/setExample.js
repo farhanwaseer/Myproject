93ff312939dd0d8ac016348276415428e4fe6a2e
farhanwaseer <farhanaliwaseer@gmail.com>
2020-10-05 11:06:02 +0500

    updated

---
+++ ES6 javaScript/class6/setExample.js
@@ -0,0 +1,13 @@
+// const newSet = new Set(['Farhan', 'Waseer','Farhan']);
+
+// console.log(newSet)
+
+const newSet = new Set();
+
+newSet.add('Muhammed');
+newSet.add('Farhan');
+newSet.add('Waseer');
+
+for(i of newSet){
+    console.log(i)
+}
