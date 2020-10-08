4d2a7ded357be997b5fba11e7a7df71c3f7e69d4
farhanwaseer <farhanaliwaseer@gmail.com>
2020-10-08 12:33:29 +0500

    updated

--- ES6 javaScript/class8/callback.js
+++ ES6 javaScript/class8/callback.js
@@ -3,14 +3,26 @@ const posts = [
     {title: 'Post two', body: 'This is post two'}
 ];
 
-function getPost() {
+function getPosts() {
     let output = '';
     setTimeout(() => {
         posts.forEach(post =>  {
-            output += `<li>${post.title}</li>`
+            output += `<li>${post.title}</li>
+            <h1>FARHAN WASEER</h1>`
             document.body.innerHTML = output; 
         })
     }, 2000)
 }
 
-getPost();
+getPosts();
+
+function createPost(post, callback) {
+    setTimeout(() => {
+        posts.push(post);
+        callback();
+    }, 2000);
+}
+
+createPost({title: 'Post three' , body: 'This is post three'}, getPosts);
+
+
