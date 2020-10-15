05abff66d0a8c2961c1586a4d44f4f62ee0a4015
farhanwaseer <farhanaliwaseer@gmail.com>
2020-10-16 00:26:13 +0500

    updt

--- ES6 javaScript/class8/Api.js
+++ ES6 javaScript/class8/Api.js
@@ -1,9 +1,29 @@
 //  Currency Api 
 
 document.addEventListener('DOMContentLoaded', function(){
-    fetch('https://api.exchangeratesapi.io/latest?base=USD')
+
+    document.querySelector('form').onsubmit = function() {
+        
+        fetch('https://api.exchangeratesapi.io/latest?base=USD')
     .then(response => response.json())
     .then(data => {
         console.log(data);
+        const currency = document.querySelector('#currency').value;
+        const rate = data.rates[currency];
+             if(rate !== undefined){
+                 document.querySelector('#result').innerHTML = `<h2>1 USD is equal to ${rate.toFixed(3)} EUR.</h2>`;  
+
+             }else{
+                document.querySelector('#result').innerHTML = 'Invalid Currency';
+             }
+    })
+
+    .catch(error => {
+     console.log('Error:', error);
+       
     });
+
+        return false;
+    }
+    
 });
