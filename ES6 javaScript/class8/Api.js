6ccb37bd40ef11ce9b82e493907522b8e8bb10da
farhanwaseer <farhanaliwaseer@gmail.com>
2020-10-16 21:45:45 +0500

    updt

--- ES6 javaScript/class8/Api.js
+++ ES6 javaScript/class8/Api.js
@@ -1,33 +1,29 @@
 //  Currency Api 
 
-document.querySelector('form').onsubmit = function() {
+document.addEventListener('DOMContentLoaded', function(){
 
-    // Send a GET request to the URL
-    fetch('https://api.exchangeratesapi.io/latest?base=USD')
-    // Put response into json form
+    document.querySelector('form').onsubmit = function() {
+        
+        fetch('https://api.exchangeratesapi.io/latest?base=USD')
     .then(response => response.json())
     .then(data => {
-        // Get currency from user input and convert to upper case
-        const currency = document.querySelector('#currency').value.toUpperCase();
-
-        // Get rate from data
+        console.log(data);
+        const currency = document.querySelector('#currency').value;
         const rate = data.rates[currency];
+             if(rate !== undefined){
+                 document.querySelector('#result').innerHTML = `<h2>1 USD is equal to ${rate.toFixed(3)} EUR.</h2>`;  
 
-        // Check if currency is valid:
-        if (rate !== undefined) {
-            // Display exchange on the screen
-            document.querySelector('#result').innerHTML = `1 USD is equal to ${rate.toFixed(3)} ${currency}.`;
-        }
-        else {
-            // Display error on the screen
-            document.querySelector('#result').innerHTML = 'Invalid Currency.';
-        }
+             }else{
+                document.querySelector('#result').innerHTML = 'Invalid Currency';
+             }
     })
-    // Catch any errors and log them to the console
+
     .catch(error => {
-        console.log('Error:', error);
+     console.log('Error:', error);
+       
     });
-    // Prevent default submission
-    return false;
-}
+
+        return false;
+    }
+    
 });
