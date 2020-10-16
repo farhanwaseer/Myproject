d4dc12d7755830f0f6f148808ca2c9363967f2b3
farhanwaseer <farhanaliwaseer@gmail.com>
2020-10-16 22:53:17 +0500

    updt

--- ES6 javaScript/class9/asycnAwait.js
+++ ES6 javaScript/class9/asycnAwait.js
@@ -1,26 +1,14 @@
-// const getPosts = async () => {
-
-//     try {
-      
-//     const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-//     const data =  await response.json();
-//     console.log('response ===>', data);
-
-//     } catch(error) { 
-//         console.log('Something went wronggggg !!!') 
-
-//     }
-     
-// }
-
-// getPosts();
-
 const getPosts = async () => {
 
-const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-const data = await response.json();
-console.log('response ==>', data);
+    try {
 
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        const data = await response.json();
+        console.log('response ==>', data);
+
+    } catch (error) {
+        console.log('SOME THING WENT WRONG')
+    }
 
 }
 
---
+++ ES6 javaScript/class9/class.js
@@ -0,0 +1,9 @@
+class Car {
+
+    constructor(model, engin, color, year) {
+        this.model = model;
+        this.engin = engin;
+        this.color = color;
+        this.year = year;
+    }
+}
