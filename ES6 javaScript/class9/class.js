b37763b9f4f9c69b4b36e03feac909deae29a488
farhanwaseer <farhanaliwaseer@gmail.com>
2020-10-17 15:26:07 +0500

    updt

--- ES6 javaScript/class9/class.js
+++ ES6 javaScript/class9/class.js
@@ -6,10 +6,31 @@ class Car {
         this.color = color;
         this.year = year;
     }
+
+    carColor() {
+        console.log(`The color is ${this.color}`)
+    }
 }
 
 const corollaGLI = new Car('GLI', 1300, 'White', 2020);
 const corollsGrande = new Car('Grande', 1600, 'black', 2020);
 
 console.log(corollaGLI);
-console.log(corollsGrande);
+corollaGLI.carColor();
+
+
+console.log(corollsGrande); 
+corollsGrande.carColor();
+  
+class Dealer extends Car {
+
+    constructor(name, address, model, engin, color, year){
+        super(model, engin, color, year)
+        this.name = name;
+        this.address = address;
+    }
+}
+
+const dealer1 = new Dealer('Haseeb Motors', 'KHI', 'GLI', 1300, 'Grey', 2020);
+
+console.log(dealer1);
